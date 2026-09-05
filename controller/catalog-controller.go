fd86709eac9eae8a10f4485adeb441894e36f890
Abhinav <abhinavsunil@hotmail.com>
2026-09-05 23:10:29 +0530

    Feat : Working on controller layer for movie geting bt required to do marshallling for json response

--- controller/catalog-controller.go
+++ controller/catalog-controller.go
@@ -1,1 +1,31 @@
 package controller
+
+import (
+	"net/http"
+	"renet-catalog/services"
+	"strconv"
+)
+
+type CatalogController struct {
+	CatalogService services.CatalogService
+}
+
+func (ctrl *CatalogController) GetMovie(w http.ResponseWriter, r *http.Request) {
+	
+	id := r.URL.Query().Get("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		http.Error(w, "Invalid movie ID", http.StatusBadRequest)
+		return
+	}
+	if id == "" {
+		http.Error(w, "Missing movie ID", http.StatusBadRequest)
+		return
+	}
+	movie,err := ctrl.CatalogService.GetMovieByID(idInt)
+	if err != nil {
+		http.Error(w, "Movie not found", http.StatusNotFound)
+		return
+	}
+	
+}
